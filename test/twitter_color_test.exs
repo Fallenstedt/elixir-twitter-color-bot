947038b56b66a64b1287c536c97a9d0058af2e81
Fallenstedt <a.fallenstedt@gmail.com>
2017-06-17 15:27:31 -0700

    add test for creating colors

--- test/twitter_color_test.exs
+++ test/twitter_color_test.exs
@@ -2,10 +2,24 @@ defmodule TwitterColorTest do
   use ExUnit.Case
   doctest TwitterColor
 
-  test "createRandomString creates a string of specified length" do
-    struct = TwitterColor.createRandomString(12)
+  test "create_random_string creates a string of specified length" do
+    struct = TwitterColor.create_random_string(12)
     string_length = String.length(struct.string)
     assert string_length == 12
+  end
+
+  test "pick_color three rgb colors are made from a crypto.hash value, and each color is a tuple of 3 numbers" do
+    image = TwitterColor.create_random_string(12)
+    |> TwitterColor.hash_input
+    |> TwitterColor.pick_color
+
+    colors = Map.values(image.color)
+    [ color1, color2, color3 ] = colors
+
+    assert length(colors) == 3
+    assert tuple_size(color1) == 3
+    assert tuple_size(color2) == 3
+    assert tuple_size(color3) == 3
   end
 
 end
