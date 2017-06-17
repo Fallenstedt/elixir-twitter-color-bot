57349bd457d60966d83c202f30f0e21e07e394bd
Fallenstedt <a.fallenstedt@gmail.com>
2017-06-17 15:12:29 -0700

    add image struct

---
+++ lib/image.ex
@@ -0,0 +1,6 @@
+defmodule TwitterColor.Image do
+  @moduledoc """
+    A struct for storing information about an image.
+  """
+  defstruct hex: nil, color: nil, grid: nil, pixel_map: nil, string: nil
+end
