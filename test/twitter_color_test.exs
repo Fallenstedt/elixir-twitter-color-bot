57c10da2bc372c3984450a2f6977cb48d9ed3731
Fallenstedt <a.fallenstedt@gmail.com>
2017-07-07 23:42:27 -0700

    add test examples

--- test/twitter_color_test.exs
+++ test/twitter_color_test.exs
@@ -1,15 +1,10 @@
 defmodule TwitterColorTest do
   use ExUnit.Case
+  import TwitterColor.CreateImage
   doctest TwitterColor
 
-  test "create_random_string creates a string of specified length" do
-    struct = TwitterColor.create_random_string(12)
-    string_length = String.length(struct.string)
-    assert string_length == 12
-  end
-
   test "pick_color produces a list with three tuples, with each tuple having a length of 3" do
-    struct = TwitterColor.CreateImage.pick_color
+    struct = pick_color()
     [ color1, color2, color3 ] = struct.color
 
     assert length(struct.color) == 3
@@ -18,4 +13,12 @@ defmodule TwitterColorTest do
     assert tuple_size(color3) == 3
   end
 
+  test "create_grid creates a list of three tuples, each have a vlue of 100," do
+    img = %TwitterColor.Image{}
+    |> create_grid
+
+    Enum.each(img.grid, fn({size, _index}) ->
+      assert size == 100
+    end)
+  end
 end
